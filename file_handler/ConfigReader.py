6b39246631d5b612c5d2a3c516931b2c35126cf9
lishang08 <1129958515@qq.com>
2017-12-05 13:57:44 +0800

    测试configparser

---
+++ file_handler/ConfigReader.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+
+import ConfigParser
+import os
+
+cf = ConfigParser.ConfigParser()
+
+cf.read('test.conf')
+
+secs = cf.sections()
+
+print('sections:', secs, type(secs))
+
+opts = cf.options('db')
+
+print('options:', opts, type(opts))
+
+kvs = cf.items('db')
+
+print('db:', kvs, type(kvs))
+
+#遍历字典
+for k, v in kvs:
+    print('{k}:{v}'.format(k=k, v=v))
+
+#read by type
+db_host = cf.get('db', 'db_host')
+db_user = cf.get('db', 'db_user')
+db_port = cf.get('db', 'db_port')
+db_pass = cf.get('db', 'db_pass')
+
+print(db_host)
+print(db_user)
+print(db_port)
+print(db_pass)
+
+#read int
+threads = cf.getint("concurrent", "thread")
+processors = cf.getint("concurrent", "processor")
+
+print('db_host:', db_host)
+print('db_user:', db_user)
+print('db_port:', db_port)
+print('db_pass:', db_pass)
+print('db_threads:', threads)
+print('db_processors:', processors)
+
+
+
