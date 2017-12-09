e3885b5f2845617915d82989c638c14c15d8f9b5
lishang08 <1129958515@qq.com>
2017-12-09 23:14:06 +0800

    console输入及跳转

--- file_handler/ExcelHandler.py
+++ file_handler/ExcelHandler.py
@@ -1,43 +1,82 @@
 # coding=utf-8
 """
 excel 读写
-支持 xls， xlsx 格式
+支持 xls， xlsx, csv 格式
 """
 
 import xlrd
+import os.path
 from StringUtil import StringUtil
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 class ExcelHandler:
 
     def readExcel(self, filename):
+
+        """读取excel"""
+
         excel = xlrd.open_workbook(filename)
         ts = len(excel.sheets())
         for i in range(0, ts):
             sheet = excel.sheet_by_index(i)
-            print(u'当前处理: %s ' % sheet.name)
+            print(u'当前处理表格: %s ' % sheet.name)
 
             rows = sheet.nrows  # 获取行数
             cols = sheet.ncols  # 获取列数
             for i in range(rows):
+                if i == 0:
+                    continue # 去掉第一行
+                row_content = ""
                 for j in range(cols):
-                    #格式化date
-                    dataType = sheet.cell(i, j).ctype
+                    dataType = sheet.cell(i, j).ctype  # 格式化date
                     if dataType == 3:
                         util = StringUtil()
-                        print(util.formatDate(sheet.cell_value(i, j), dataType))
+                        row_content = row_content+'|'+str(util.formatDate(sheet.cell_value(i, j), dataType))
                     else:
-                        print(sheet.cell_value(i, j))
+                        row_content = row_content+'|'+str(sheet.cell_value(i, j))
+                print(row_content)
 
                 print("--------------")
 
             print("=============")
 
 
+    def route(self, filename):
+
+        """判断文件格式，只允许xls, xlsx, csv 格式"""
+
+        fileSuffix = os.path.splitext(filename)[1].lower()
+        print("Current input file suffix is : %s" % fileSuffix)
+
+        if fileSuffix == '.xls' or fileSuffix == '.xlsx':
+            self.readExcel(filename)
+        elif fileSuffix == '.csv':
+            print('will support csv in next version')
+        else:
+            print('Only support excel format')
+
+    def input(self, str):
+
+        """根据输入提前判断是否继续下一步"""
+
+        if str == 'stop':
+            sys.exit()
+        else:
+            if os.path.exists(filename):
+                excelHandler.route(filename)
+            else:
+                print('找不到文件 %s , 请重试！' % filename)
+                sys.exit()
+
+
 
 if __name__ == '__main__':
 
     excelHandler = ExcelHandler()
-    excelHandler.readExcel('management.xls')
+    filename = raw_input("Input file: ")
+    excelHandler.input(filename)
 
 
 
